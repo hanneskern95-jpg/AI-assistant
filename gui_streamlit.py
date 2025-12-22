c1f958861e20dc0d8a4e4f0388bc0347a65f55da
HannesKern <hannes.kern95@gmail.com>
2025-12-22 23:31:50 +0100

    Added a max height, so the pinned object does not dissappear.

--- gui_streamlit.py
+++ gui_streamlit.py
@@ -11,11 +11,13 @@ def show_chat() -> None:
 
     #Display chat history
     chat, pinned_object = st.tabs(["Chat", "Pinned Object"])
+
     with chat:
-        messages_to_display = [message for message in st.session_state.chat_assistant.history if message["role"] in ["user", "assistant", "tool"] and "tool_calls" not in message]
-        for message in messages_to_display:
-            with st.chat_message(message["role"]):
-                st.session_state.chat_assistant.render_answer(message)
+        with st.container(height=470, border=False):
+            messages_to_display = [message for message in st.session_state.chat_assistant.history if message["role"] in ["user", "assistant", "tool"] and "tool_calls" not in message]
+            for message in messages_to_display:
+                with st.chat_message(message["role"]):
+                    st.session_state.chat_assistant.render_answer(message)
 
     with pinned_object:
         if st.session_state["pinned_object"] is None:
